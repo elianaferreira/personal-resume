610b8e609e5d1a38ffc857030085873caa759c57
Eliana Ferreira <elianaef817@gmail.com>
2021-05-30 16:45:03 -0400

    padding at the bottom of certificates section

--- components/certificates.js
+++ components/certificates.js
@@ -6,7 +6,7 @@ export default function Certificates({ certificates }) {
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
         Certificates
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-10 lg:gap-x-32 gap-y-10 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-10 lg:gap-x-32 gap-y-10 mb-10">
         {certificates.map((cert) => (
           <Box
             title={cert.title}
